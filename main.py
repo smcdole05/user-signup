0c3950ff3f75a00575b7f9ebc1d8b8429520b76e
Spencer McDole <spmd05@gmail.com>
2019-04-15 19:31:54 -0500

    Fixed password confirmation and email error logic

--- main.py
+++ main.py
@@ -32,32 +32,26 @@ def validate_signup():
     #do not leave blank
     #must be between 3 and 20 characters, no spaces
     #perserve what the user types
-    if username == '':
-        username_error = "That is not a valid username"
-        username = ''
-    elif " " in username or len(username) < 3 or len(username) > 20:
-        username_error = "Username must not contain spaces, and must be between 3-20 characters"
+    if " " in username or len(username) < 3 or len(username) > 20:
+        username_error = "Username must not contain spaces and must be between 3-20 characters"
         username = ''
 
     #password 
     #same requirements as username
     #must match with password confirmation
     #clear field if error occurs
-    if password == '':
-        password_error = "That is not a valid password"
-        password = ''
-    elif " " in password or len(password) < 3 or len(password) > 20:
-        password_error = "Username must not contain spaces, and must be between 3-20 characters"
+    if " " in password or len(password) < 3 or len(password) > 20:
+        password_error = "Password must not contain spaces and must be between 3-20 characters"
         password = ''
 
     #additional password verification field
     #must match with password
     #clear field if error occurs
-    if password_check == '':
+    if not password_check == password:
         password_check_error = "That password does not match"
         password_check = ''
     elif " " in password_check or len(password_check) < 3 or len(password_check) > 20:
-        password_check_error = "That password does not match"
+        password_check_error = "Password must not contain spaces and must be between 3-20 characters"
         password_check = ''
 
     #optionally an email
@@ -68,25 +62,27 @@ def validate_signup():
     #between 3 and 20 characters long
     #preserve what the user types
     if len(email) > 0 and len(email) < 3 or len(email) > 20:
-        email_error = "Email length must be between 3 and 20 characters"
+        email_error = "Email must contain no spaces and must be between 3 and 20 characters"
         email = ""
-    elif " " in email:
-        email_error = "Email must not contain spaces"
-        email = ""
+    #elif len(email) > 0 and not "@" in email or not "." in email:
+    #    email_error = "Email must be properly formatted with @ and (.)."
     else:
         symbol_count = 0
         period_count = 0
-        for char in email:
-            if char == '@':
-                symbol_count += 1
-            if symbol_count > 1:
-                email_error = "Email must contain no more than one @ or ."
-                email = ""
-            if char == '.':
-                period_count += 1
-            if period_count > 1:
-                email_error = "Email must contain no more than one @ or ."
-                email = ""
+        if "@" in email and "." in email:
+            for char in email:
+                if char == '@':
+                    symbol_count += 1
+                if symbol_count > 1:
+                    email_error = "Email must contain no more than one @ or ."
+                    email = ""
+                if char == '.':
+                    period_count += 1
+                if period_count > 1:
+                    email_error = "Email must contain no more than one @ or ."
+                    email = ""
+        elif len(email) > 0:
+            email_error = "Email must be properly formatted with a @ and ."
     
     if not username_error and not password_error and not password_check_error and not email_error:
         return redirect('/welcome?user_name={0}'.format(username))
